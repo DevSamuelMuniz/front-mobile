ff606921169b6c6b3e9c4ca1bc46083f200bdf9a
SamuelFecomercio <156712738+SamuelFecomercio@users.noreply.github.com>
2024-04-07 10:53:47 -0300

    Start React Project - NutriLife

---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import './home.css';
+import Header from '../../components/header/header';
+
+function Home(){
+    return(
+        <main>
+            <Header />
+        </main>
+    );
+}
+
+export default Home
