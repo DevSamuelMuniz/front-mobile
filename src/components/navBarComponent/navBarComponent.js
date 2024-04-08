aac058057480d241d9795ad05a9d21760efef04e
SamuelFecomercio <156712738+SamuelFecomercio@users.noreply.github.com>
2024-04-08 16:56:32 -0300

    navBar

---
+++ src/components/navBarComponent/navBarComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//css
+import "./navBarComponent.css"
+
+function navBarComponent(){
+    return(
+        <main>
+            <a href=""></a>
+        </main>
+    );
+};
+
+export default  navBarComponent; 
