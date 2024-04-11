004988d71566f3d81f267c770d3f67d9aa192835
SamuelFecomercio <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-04-11 17:02:52 -0300

    add api comidas

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,15 +1,40 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./homePage.css";
-//css
-import "./homePage.css";
-//components
 import HeaderComponent from "../../components/headerComponent/headerComponent";
-import RecipeCarrosselComponent from "../../components/recipeCarrosselComponent/recipeCarrosselComponent";
-
-//imagens
 import Banner from "../../assets/img/banner.png";
 
 function Home() {
+  const [recipes, setRecipes] = useState([]); // Estado para armazenar as receitas
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  // Função para buscar as receitas da API
+  const fetchRecipes = async () => {
+    try {
+      // Lógica para buscar as receitas da API da Edamam
+      // Substitua esta lógica pela sua própria chamada à API
+      const response = await fetch(
+        "https://api.edamam.com/search?q=health-lunch&app_id=cfff52eb&app_key=2e15b4a09b6e798f9f51590b31214651&language=pt"
+      );
+      const data = await response.json();
+      setRecipes(data.hits); // Assume que a resposta da API retorna uma matriz de receitas
+    } catch (error) {
+      console.error("Erro ao buscar receitas:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+  const handleRecipeClick = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedRecipe(null);
+  };
+
+  //modal
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <main className="content-header">
@@ -17,8 +42,35 @@ function Home() {
       <div className="banner-content">
         <img className="banner1" src={Banner} alt="imagem banner" />
       </div>
-      <RecipeCarrosselComponent />
-      <RecipeCarrosselComponent />
+
+      <div className="receitas">
+        <h1>Receitas</h1>
+        <div className="foto-receita">
+          {/* Galeria de imagens das receitas */}
+          {recipes.map((recipe, index) => (
+            <img
+              key={index}
+              src={recipe.recipe.image}
+              alt={recipe.recipe.label}
+              onClick={() => handleRecipeClick(recipe.recipe)}
+            />
+          ))}
+          {/* Modal de detalhes da receita */}
+          {selectedRecipe && (
+            <div>
+              <h2>{selectedRecipe.label}</h2>
+              <img src={selectedRecipe.image} alt={selectedRecipe.label} />
+              <ul>
+                {selectedRecipe.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient.text}</li>
+                ))}
+              </ul>
+              <p>Instruções: {selectedRecipe.instructions}</p>
+              <button onClick={handleCloseModal}>Fechar</button>
+            </div>
+          )}
+        </div>
+      </div>
     </main>
   );
 }
