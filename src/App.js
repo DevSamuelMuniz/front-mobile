e12b007487911aeaf363e305e51844feec4b461b
SamuelFecomercio <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-04-14 19:53:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import axios from "axios";
 
 //splashScreen
 import SplashScreenComponent from "./components/splashScreenComponent/splashScreenComponent";
@@ -15,32 +11,44 @@ import HomePage from "./pages/homePage/homePage";
 import RegistroPage from "./pages/registroPage/registroPage";
 import ProgressPage from "./pages/progressPage/progressPage";
 import AddRefPage from "./pages/addRefPage/addRefPage";
-import RefeicoesPage from "./pages/refeicoesPage/refeicoesPage";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 2000);
+
+    // Verificar se o usuário está logado ao carregar a aplicação
+    checkLoggedIn();
   }, []);
+
+  // Função para verificar se o usuário está logado
+  const checkLoggedIn = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/users");
+      if (response.data.length > 0) {
+        setLoggedIn(true);
+      }
+    } catch (error) {
+      console.error("Erro ao verificar login:", error);
+    }
+  };
 
   return (
     <Router>
       <Routes>
         <Route
           path="/"
-          element={
-            loading ? <SplashScreenComponent /> : <Navigate to="/login" />
-          }
+          element={loading ? <SplashScreenComponent /> : loggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />}
         />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/cadastro" element={<RegistroPage />} />
         <Route path="/progresso" element={<ProgressPage />} />
         <Route path="/adicionar" element={<AddRefPage />} />
-        <Route path="/refeicoes" element={<RefeicoesPage />} />
       </Routes>
     </Router>
   );
