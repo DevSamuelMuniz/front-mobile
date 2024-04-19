09760728f68c8049816231a00d69ab72858fdc54
SamuelFecomercio <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-04-19 10:07:30 -0300

    update

--- src/pages/addRefPage/addRefPage.js
+++ src/pages/addRefPage/addRefPage.js
@@ -8,10 +8,10 @@ import { useNavigate } from "react-router-dom";
 function AddRefPage() {
   const [mealName, setMealName] = useState("");
   const [mealDescription, setMealDescription] = useState("");
-  const [pic, setpic] = useState(null);
+  const [pic, setPic] = useState(null);
   const navigate = useNavigate();
 
-  const handleAddpicClick = () => {
+  const handleAddPicClick = () => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
         .getUserMedia({ video: true })
@@ -32,8 +32,8 @@ function AddRefPage() {
                 context.drawImage(video, 0, 0, canvas.width, canvas.height);
                 const dataUrl = canvas.toDataURL("image/jpeg");
 
-                // Define a imagem capturada no estado1
-                setpic(dataUrl);
+                // Define a imagem capturada no estado
+                setPic(dataUrl);
 
                 // Limpa os recursos
                 stream.getTracks().forEach((track) => track.stop());
@@ -60,48 +60,27 @@ function AddRefPage() {
   };
 
   const handleSaveClick = () => {
-    const blob = dataURLtoBlob(pic);
-
-    // Crie um FormData para enviar a imagem junto com outros dados
-    const formData = new FormData();
-    formData.append("userId", localStorage.getItem("token"));
-    formData.append("title", mealName);
-    formData.append("description", mealDescription);
-    formData.append("pic", blob);
-  
     axios
-      .post("http://localhost:5000/api/post", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
+      .post("http://localhost:5000/api/post", {
+        userId: localStorage.getItem("token"),
+        title: mealName,
+        description: mealDescription,
+        pic: pic,
       })
       .then((response) => {
         console.log("Refeição salva com sucesso!");
         setMealName("");
         setMealDescription("");
-        setpic(null);
+        setPic(null);
         // Check if the response indicates success
         if (response.data.message === "Post saved successfully") {
           // Redirect to /refeicoes
-          navigate("/refeicoes"); // Corrected navigation
+          navigate("/refeicoes");
         }
       })
       .catch((error) => {
         console.error("Erro ao salvar a refeição:", error);
       });
-  };
-  
-  // Função para converter uma string base64 em um blob
-  function dataURLtoBlob(dataURL) {
-    const arr = dataURL.split(",");
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], { type: mime });
   };
 
   return (
@@ -122,7 +101,7 @@ function AddRefPage() {
         onChange={(e) => setMealDescription(e.target.value)}
       ></textarea>
       {pic && <img src={pic} alt="Refeição" className="photo-preview" />}
-      <button className="btn-add" onClick={handleAddpicClick}>
+      <button className="btn-add" onClick={handleAddPicClick}>
         <img src={Camera} alt="Camera"></img>
         <span>Adicionar foto da Refeição</span>
       </button>
@@ -135,4 +114,3 @@ function AddRefPage() {
 }
 
 export default AddRefPage;
-
